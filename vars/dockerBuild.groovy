a2a55fb3430dcc15e98fe2c88c32d21002315c5b
Ghada Houissa <ghadahouissa0@gmail.com>
2024-10-28 05:52:50 +0100

    dockers

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,7 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "sudo docker build --build-arg REACT_APP_RAPID_API_KEY=3018c17b5cmsh11257f7e0e72c98p1971ebjsnc60e36e3adde -t ${imageName} ."
+
+    sh "echo 'ubuntu' | sudo -S docker build --build-arg REACT_APP_RAPID_API_KEY=3018c17b5cmsh11257f7e0e72c98p1971ebjsnc60e36e3adde -t ${imageName} ."
      // Tag the Docker image
     sh "sudo docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
