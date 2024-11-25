9b92fc8542392e338a007a7fa764101a655e084d
g-code <84587070+ghada-code@users.noreply.github.com>
2024-11-25 02:16:59 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,14 +1,24 @@
 def call(String dockerHubUsername, String imageName) {
-    sh "echo "dockerHubUsername: ${dockerHubUsername}, imageName: ${imageName}""
+    // Login to Docker Hub
+    withCredentials([usernamePassword(credentialsId: 'docker', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh """
+            echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin
+        """
+    }
 
-sh """
-    sudo docker build \
-    --progress=plain \
-    --build-arg REACT_APP_RAPID_API_KEY=3018c17b5cmsh11257f7e0e72c98p1971ebjsnc60e36e3adde \
-    -t ${dockerHubUsername}/${imageName}:latest .
-"""
+    // Build the Docker image
+    sh """
+        sudo docker build \
+        --progress=plain \
+        --build-arg REACT_APP_RAPID_API_KEY=3018c17b5cmsh11257f7e0e72c98p1971ebjsnc60e36e3adde \
+        -t ${dockerHubUsername}/${imageName}:latest .
+    """
 
-    withDockerRegistry(credentialsId: 'docker') {
-        sh "docker push ${dockerHubUsername}/${imageName}:latest"
+    // Debugging output
+    sh "echo 'dockerHubUsername: ${dockerHubUsername}, imageName: ${imageName}'"
+
+    // Push the Docker image
+    withDockerRegistry([credentialsId: 'docker']) {
+        sh "sudo docker push ${dockerHubUsername}/${imageName}:latest"
     }
 }
