90138cb3bfde0e7150b3ccbfd9569bc605b1b3b2
g-code <84587070+ghada-code@users.noreply.github.com>
2024-11-24 20:12:13 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,13 +1,13 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image with progress and the build argument for the API key
     sh """
-        docker build --progress=plain --build-arg REACT_APP_RAPID_API_KEY=3018c17b5cmsh11257f7e0e72c98p1971ebjsnc60e36e3adde -t ${dockerHubUsername}/${imageName}:latest .
+        sudo docker build --progress=plain --build-arg REACT_APP_RAPID_API_KEY=3018c17b5cmsh11257f7e0e72c98p1971ebjsnc60e36e3adde -t ${dockerHubUsername}/${imageName}:latest .
     """
     
     // Push the Docker image to Docker Hub with the correct credentials
     withDockerRegistry([credentialsId: 'docker']) {
         sh """
-            docker push ${dockerHubUsername}/${imageName}:latest
+            sudo docker push ${dockerHubUsername}/${imageName}:latest
         """
     }
 }
